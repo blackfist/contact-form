a839fc6def14d6035d58cc05bc9ff49ff6397e5c
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-11-02 15:31:19 +0000

    thanks the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/thanks'
+
   resources :forms
   root 'forms#new'
   # The priority is based upon order of creation: first created -> highest priority.
